c508cc37fe25a22b5e336803497022e164600663
laurent-rochette <laurent.rochette@servicenow.com>
2020-04-29 16:39:04 -0500

    Adding script

---
+++ TC/vars.sh
@@ -0,0 +1,7 @@
+export INSTANCENAME='lrochette1'
+export TOOLID='e6ae5961db2d0850e1155c55dc9619ed'
+
+export URLSTEPMAP="https://$INSTANCENAME.service-now.com/api/sn_devops/v1/devops/orchestration/stepMapping"
+export URLNOTIF="https://$INSTANCENAME.service-now.com/api/sn_devops/v1/devops/tool/orchestration"
+export URLCC="https://$INSTANCENAME.service-now.com/api/sn_devops/v1/devops/orchestration/changeControl"
+export SNUSER='devops.integration.user'
