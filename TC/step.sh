c1637a77ef1e846eadd675b69d794765f5a09d45
laurent-rochette <laurent.rochette@servicenow.com>
2020-04-29 17:56:48 -0500

    Fix syntax for system property

--- TC/step.sh
+++ TC/step.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 echo "TC: Starting Step registration"
-echo "TC shell: $SHELL"
-pwd
-ls -ail
 source TC/vars.sh
-
 echo "TC: Environment variable read"
 env
 
 echo "TC: Starting Step registration"
 
 echo "Mapping for stage $CI_JOB_STAGE"
-export BODY="{\"branchName\":\"${teamcity.build.branch}\","
+export BODY="{\"branchName\":\"%teamcity.build.branch%\","
 BODY+="\"orchestrationTaskName\":\"${TEAMCITY_PROJECT_NAME}#$CI_JOB_STAGE\","
 BODY+="\"orchestrationTaskURL\":\"$ORCHESTRATION_TASK_URL#$CI_JOB_STAGE\"}"
 echo "BODY $BODY"
