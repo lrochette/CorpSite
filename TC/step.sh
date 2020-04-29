237d7f698403fef0531256a70306201553f0782d
laurent-rochette <laurent.rochette@servicenow.com>
2020-04-29 17:24:30 -0500

    Adding step mapping

--- TC/step.sh
+++ TC/step.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-echo "Starting Step registration"
-echo $SHELL
+echo "TC: Starting Step registration"
+echo "TC shell: $SHELL"
 pwd
+ls -ail
 source TC/vars.sh
 
-echo "Environment variable read"
+echo "TC: Environment variable read"
 env
+
+echo "TC: Starting Step registration"
+
+echo "Mapping for stage $CI_JOB_STAGE"
+export BODY="{\"branchName\":\"${teamcity.build.branch}\","
+BODY+="\"orchestrationTaskName\":\"${TEAMCITY_PROJECT_NAME}#$CI_JOB_STAGE\","
+BODY+="\"orchestrationTaskURL\":\"$ORCHESTRATION_TASK_URL#$CI_JOB_STAGE\"}"
+echo "BODY $BODY"
+curl -X POST -u ${SNUSER}:$SNPWD -H "Content-Type:application/json" "$URLSTEPMAP?toolId=$TOOLID&toolType=GitLab" --data "$BODY"
