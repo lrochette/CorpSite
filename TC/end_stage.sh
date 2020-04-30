215d11492a625ec691efceec13c07c101fea81b3
laurent-rochette <laurent.rochette@servicenow.com>
2020-04-30 15:21:21 -0500

    refactor code for scripts

---
+++ TC/end_stage.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source TC/vars.sh
+
+complete_stage()
--- TC/vars.sh
+++ TC/vars.sh
@@ -8,3 +8,43 @@ export SNUSER='devops.integration.user'
 
 export CI_JOB_STAGE="Build"
 export ORCHESTRATION_TASK_URL="http://teamcity:8111/viewType.html?buildTypeId=CorpSiteTC_Ci"
+export CI_BRANCH="master"
+
+function step_mapping()  {
+  echo "Mapping for stage $CI_JOB_STAGE"
+  export BODY="{\"branchName\":\"${CI_BRANCH}\","
+  BODY+="\"orchestrationTaskName\":\"${TEAMCITY_PROJECT_NAME}#$CI_JOB_STAGE\","
+  BODY+="\"orchestrationTaskURL\":\"$ORCHESTRATION_TASK_URL#$CI_JOB_STAGE\"}"
+  echo "BODY $BODY"
+  curl -X POST -u ${SNUSER}:$SNPWD -H "Content-Type:application/json" "$URLSTEPMAP?toolId=$TOOLID&toolType=GitLab" --data "$BODY"
+}
+
+function start_stage() {
+  echo "Notification STARTED"
+  export BODY="{\"toolId\":\"$TOOLID\",\"buildNumber\":\"$CI_PIPELINE_ID\","
+  BODY+="\"nativeId\":\"${CI_PROJECT_NAME}/${CI_JOB_STAGE}#$CI_CONCURRENT_ID\",\"name\":\"${CI_PROJECT_NAME}/${CI_JOB_STAGE}\","
+  BODY+="\"id\":\"${CI_PROJECT_NAME}/${CI_JOB_STAGE}#$CI_CONCURRENT_ID\",\"url\":\"$CI_JOB_URL/\","
+  BODY+="\"isMultiBranch\":\"false\",\"orchestrationTaskUrl\":\"$ORCHESTRATION_TASK_URL#$CI_JOB_STAGE\","
+  BODY+="\"orchestrationTaskName\":\"${CI_PROJECT_NAME}#${CI_JOB_STAGE}\","
+  BODY+="\"upstreamTaskUrl\":\"$UPSTREAM_URL\",\"upstreamId\":\"$UPSTREAM_ID\","
+  BODY+="\"result\":\"building\",\"startDateTime\":\""
+  BODY+=`date +'%Y-%m-%d %H:%M:%S'`
+  BODY+="\"}"
+  echo $BODY
+  curl -X POST -u ${SNUSER}:$SNPWD -H "Content-Type:application/json" "$URLNOTIF?toolId=$TOOLID&toolType=GitLab" --data "$BODY"
+}
+
+fcuntion end_stage() {
+  echo "Notification COMPLETED"
+  export BODY="{\"toolId\":\"$TOOLID\",\"buildNumber\":\"$CI_PIPELINE_ID\","
+  BODY+="\"nativeId\":\"${CI_PROJECT_NAME}/${CI_JOB_STAGE}#$CI_CONCURRENT_ID\",\"name\":\"${CI_PROJECT_NAME}/${CI_JOB_STAGE}\","
+  BODY+="\"id\":\"${CI_PROJECT_NAME}/${CI_JOB_STAGE}#$CI_CONCURRENT_ID\",\"url\":\"$CI_JOB_URL/\","
+  BODY+="\"isMultiBranch\":\"false\",\"orchestrationTaskUrl\":\"$ORCHESTRATION_TASK_URL#$CI_JOB_STAGE\","
+  BODY+="\"orchestrationTaskName\":\"${CI_PROJECT_NAME}#${CI_JOB_STAGE}\","
+  BODY+="\"upstreamTaskUrl\":\"$UPSTREAM_URL\",\"upstreamId\":\"$UPSTREAM_ID\","
+  BODY+="\"result\":\"successful\",\"endDateTime\":\""
+  BODY+=`date +'%Y-%m-%d %H:%M:%S'`
+  BODY+="\"}"
+  echo $BODY
+  curl -X POST -u ${SNUSER}:$SNPWD -H "Content-Type:application/json" "$URLNOTIF?toolId=$TOOLID&toolType=GitLab" --data "$BODY"
+}
