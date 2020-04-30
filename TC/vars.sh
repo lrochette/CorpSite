c5199838659a049b044da51d886ab1abcbaedc4e
laurent-rochette <laurent.rochette@servicenow.com>
2020-04-30 17:00:20 -0500

    Rename orchestration task

--- TC/vars.sh
+++ TC/vars.sh
@@ -1,3 +1,12 @@
+# Configuration parameters
+#   teamcity.project.id	  CorpSiteTC                  $TEAMCITY_PROJECT_NAME
+#   teamcity.build.id	    46
+
+# System properties
+#   system.teamcity.buildConfName	  CI                $TEAMCITY_BUILDCONF_NAME
+#   system.teamcity.buildType.id	  CorpSiteTC_Ci
+#   system.build.number	            37
+
 export INSTANCENAME='lrochette1'
 export TOOLID='4c00e9a81bac5410a9aec88c0a4bcb72'
 
@@ -8,43 +17,44 @@ export SNUSER='devops.integration.user'
 
 export STAGE="Build"
 export BRANCH="master"
-export ORCHESTRATION_TASK_URL="http://teamcity:8111/viewType.html?buildTypeId=CorpSiteTC_Ci"
+export PIPELINE_NAME="${TEAMCITY_PROJECT_NAME}_${TEAMCITY_BUILDCONF_NAME}"
+export ORCHESTRATION_TASK_URL="http://teamcity:8111/buyildConfiguration/$PIPELINE_NAME"
 
 function step_mapping()  {
   echo "Mapping for stage $STAGE"
   export BODY="{\"branchName\":\"${BRANCH}\","
-  BODY+="\"orchestrationTaskName\":\"${TEAMCITY_PROJECT_NAME}#$STAGE\","
+  BODY+="\"orchestrationTaskName\":\"${$PIPELINE_NAME}#$STAGE\","
   BODY+="\"orchestrationTaskURL\":\"$ORCHESTRATION_TASK_URL#$STAGE\"}"
   echo "BODY $BODY"
-  curl -X POST -u ${SNUSER}:$SNPWD -H "Content-Type:application/json" "$URLSTEPMAP?toolId=$TOOLID&toolType=GitLab" --data "$BODY"
+  curl -X POST -u ${SNUSER}:$SNPWD -H "Content-Type:application/json" "$URLSTEPMAP?toolId=$TOOLID&toolType=TeamCity" --data "$BODY"
 }
 
 function start_stage() {
   echo "Notification STARTED"
-  export BODY="{\"toolId\":\"$TOOLID\",\"buildNumber\":\"$BUILD_NUMBER\","
-  BODY+="\"nativeId\":\"${TEAMCITY_PROJECT_NAME}/${STAGE}#$CI_CONCURRENT_ID\",\"name\":\"${TEAMCITY_PROJECT_NAME}/${STAGE}\","
-  BODY+="\"id\":\"${TEAMCITY_PROJECT_NAME}/${STAGE}#$CI_CONCURRENT_ID\",\"url\":\"$CI_JOB_URL/\","
+  export BODY="{\"toolId\":\"$TOOLID\",\"buildNumber\":\"$BUILDID\","
+  BODY+="\"nativeId\":\"${PIPELINE_NAME}/${STAGE}#$CI_CONCURRENT_ID\",\"name\":\"${PIPELINE_NAME}/${STAGE}\","
+  BODY+="\"id\":\"${PIPELINE_NAME}/${STAGE}#$CI_CONCURRENT_ID\",\"url\":\"$CI_JOB_URL/\","
   BODY+="\"isMultiBranch\":\"false\",\"orchestrationTaskUrl\":\"$ORCHESTRATION_TASK_URL#$STAGE\","
-  BODY+="\"orchestrationTaskName\":\"${TEAMCITY_PROJECT_NAME}#${STAGE}\","
+  BODY+="\"orchestrationTaskName\":\"${PIPELINE_NAME}#${STAGE}\","
   BODY+="\"upstreamTaskUrl\":\"$UPSTREAM_URL\",\"upstreamId\":\"$UPSTREAM_ID\","
   BODY+="\"result\":\"building\",\"startDateTime\":\""
   BODY+=`date +'%Y-%m-%d %H:%M:%S'`
   BODY+="\"}"
   echo $BODY
-  curl -X POST -u ${SNUSER}:$SNPWD -H "Content-Type:application/json" "$URLNOTIF?toolId=$TOOLID&toolType=GitLab" --data "$BODY"
+  curl -X POST -u ${SNUSER}:$SNPWD -H "Content-Type:application/json" "$URLNOTIF?toolId=$TOOLID&toolType=TeamCity" --data "$BODY"
 }
 
 function end_stage() {
   echo "Notification COMPLETED"
-  export BODY="{\"toolId\":\"$TOOLID\",\"buildNumber\":\"$BUILD_NUMBER\","
-  BODY+="\"nativeId\":\"${TEAMCITY_PROJECT_NAME}/${STAGE}#$CI_CONCURRENT_ID\",\"name\":\"${TEAMCITY_PROJECT_NAME}/${STAGE}\","
-  BODY+="\"id\":\"${TEAMCITY_PROJECT_NAME}/${STAGE}#$CI_CONCURRENT_ID\",\"url\":\"$CI_JOB_URL/\","
+  export BODY="{\"toolId\":\"$TOOLID\",\"buildNumber\":\"$BUILDID\","
+  BODY+="\"nativeId\":\"${PIPELINE_NAME}/${STAGE}#$CI_CONCURRENT_ID\",\"name\":\"${PIPELINE_NAME}/${STAGE}\","
+  BODY+="\"id\":\"${PIPELINE_NAME}/${STAGE}#$CI_CONCURRENT_ID\",\"url\":\"$CI_JOB_URL/\","
   BODY+="\"isMultiBranch\":\"false\",\"orchestrationTaskUrl\":\"$ORCHESTRATION_TASK_URL#$STAGE\","
-  BODY+="\"orchestrationTaskName\":\"${TEAMCITY_PROJECT_NAME}#${STAGE}\","
+  BODY+="\"orchestrationTaskName\":\"${PIPELINE_NAME}#${STAGE}\","
   BODY+="\"upstreamTaskUrl\":\"$UPSTREAM_URL\",\"upstreamId\":\"$UPSTREAM_ID\","
   BODY+="\"result\":\"successful\",\"endDateTime\":\""
   BODY+=`date +'%Y-%m-%d %H:%M:%S'`
   BODY+="\"}"
   echo $BODY
-  curl -X POST -u ${SNUSER}:$SNPWD -H "Content-Type:application/json" "$URLNOTIF?toolId=$TOOLID&toolType=GitLab" --data "$BODY"
+  curl -X POST -u ${SNUSER}:$SNPWD -H "Content-Type:application/json" "$URLNOTIF?toolId=$TOOLID&toolType=TeamCity" --data "$BODY"
 }
